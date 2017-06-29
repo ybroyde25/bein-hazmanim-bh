3ea8704a85b021c0fdbdd05c28e4b03abe1c6a0a
Tehila Broyde <Tehila Broyde>
2017-06-29 17:05:25 -0400

    just messing around

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 console.log("git")
+console.log("git added")
+=======
+console.log("git");
+ console.log("git added");
+ gfcgjcgbvnvnbvnbvnbvnbvbvbgvg
+>>>>>>> parent of 00625f0... revert back in time
